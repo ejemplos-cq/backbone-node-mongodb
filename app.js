6080a4893f0a3c2c0289006795f86ae48ec54920
OpenShift guest <cquevedoz@gmail.com>
2014-05-27 10:42:16 -0400

    capítulo 8

--- app.js
+++ app.js
@@ -50,216 +50,222 @@ app.use(session({
     store: new MemoryStore()
 }));
 
-/*app.get('/', function(req, res){    
-    console.log('empieza esto');
-    res.render("index", {layout:false}); 
-});
-
-app.get('/account/authenticated', function(req, res) {
-    if (req.session.loggedIn) {
-        res.send(200);
-    }
-    else {
-        res.send(401);
-    }
-}); 
-
-app.post('/register', function(req, res) {
-    var firstName = req.param('firstName', '');
-    var lastName = req.param('lastName', '');
-    var email = req.param('email', null);
-    var password = req.param('password', null);
-    if (null == email || null == password) {
-        res.send(400);
-        return;
-    }
-    Account.register(email, password, firstName, lastName);
-    res.send(200);
+app.get('/', function(req, res){
+  res.render('index.jade');
 });
 
 app.post('/login', function(req, res) {
-    console.log('login request');
-    var email = req.param('email', null);
-    var password = req.param('password', null);
-    if (null == email || email.length < 1 || null == password || password.length < 1) {
-        res.send(400);
-        return;
+  console.log('login request');
+  var email = req.param('email', null);
+  var password = req.param('password', null);
+
+  if ( null == email || email.length < 1
+      || null == password || password.length < 1 ) {
+    res.send(400);
+    return;
+  }
+
+  models.Account.login(email, password, function(account) {
+    if ( !account ) {
+      res.send(401);
+      return;
     }
-    Account.login(email, password, function(success) {
-        if (!success) {
-            res.send(401);
-            return;
-        }
-        console.log('login was successful');
-        res.send(200);
-    });
-});
-
-app.post('/forgotpassword', function(req, res) {
-    var hostname = req.headers.host;
-    var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
-    var email = req.param('email', null);
-    if (null == email || email.length < 1) {
-        res.send(400);
-        return;
-    }
-    Account.forgotPassword(email, resetPasswordUrl, function(success) {
-        if (success) {
-            res.send(200);
-        }
-        else { // Username or password not found      
-            res.send(404);
-        }
-    });
-});
-
-app.get('/resetPassword', function(req, res) {
-    var accountId = req.param('account', null);
-    res.render('resetPassword.jade', {
-        locals: {
-            accountId: accountId
-        },
-        layout:false
-    });
-});
-
-app.post('/resetPassword', function(req, res) {
-    var accountId = req.param('accountId', null);
-    var password = req.param('password', null);
-    if (null != accountId && null != password) {
-        Account.changePassword(accountId, password);
-    }
-    res.render('resetPasswordSuccess.jade');
-});
-
-app.get('/accounts/:id', function(req, res) {
-    var accountId = req.params.id == 'me' ? req.session.accountId : req.params.id;
-    Account.findOne({
-        _id: accountId
-    }, function(account) {
-        res.send(account);
-    });
-});
-
-app.get('/accounts/:id/activity', function(req, res) {
-    var accountId = req.params.id == 'me' ? req.session.accountId : req.params.id;
-    models.Account.findById(accountId, function(account) {
-        res.send(account.activity);
-    });
-});
-*/
-
-app.get('/', function(req, res) {
-    res.render('index.jade');
-});
-app.post('/login', function(req, res) {
-    console.log('login request');
-    var email = req.param('email', null);
-    var password = req.param('password', null);
-    if (null == email || email.length < 1 || null == password || password.length < 1) {
-        res.send(400);
-        return;
-    }
-    models.Account.login(email, password, function(account) {
-        if (!account) {
-            res.send(401);
-            return;
-        }
-        console.log('login was successful');
-        req.session.loggedIn = true;
-        req.session.accountId = account._id;
-        res.send(200);
-    });
-});
-app.post('/register', function(req, res) {
-    var firstName = req.param('firstName', '');
-    var lastName = req.param('lastName', '');
-    var email = req.param('email', null);
-    var password = req.param('password', null);
-    if (null == email || email.length < 1 || null == password || password.length < 1) {
-        res.send(400);
-        return;
-    }
-    models.Account.register(email, password, firstName, lastName);
+    console.log('login was successful');
+    req.session.loggedIn = true;
+    req.session.accountId = account._id;
     res.send(200);
+  });
 });
+
+app.post('/register', function(req, res) {
+  var firstName = req.param('firstName', '');
+  var lastName = req.param('lastName', '');
+  var email = req.param('email', null);
+  var password = req.param('password', null);
+
+  if ( null == email || email.length < 1
+       || null == password || password.length < 1 ) {
+    res.send(400);
+    return;
+  }
+
+  models.Account.register(email, password, firstName, lastName);
+  res.send(200);
+});
+
 app.get('/account/authenticated', function(req, res) {
-    if (req.session.loggedIn) {
-        res.send(200);
-    }
-    else {
-        res.send(401);
-    }
+  if ( req.session.loggedIn ) {
+    res.send(200);
+  } else {
+    res.send(401);
+  }
 });
+
+app.get('/accounts/:id/contacts', function(req, res) {
+  var accountId = req.params.id == 'me'
+                     ? req.session.accountId
+                     : req.params.id;
+  models.Account.findById(accountId, function(account) {
+    res.send(account.contacts);
+  });
+});
+
 app.get('/accounts/:id/activity', function(req, res) {
-    var accountId = req.params.id == 'me' ? req.session.accountId : req.params.id;
-    models.Account.findById(accountId, function(account) {
-        res.send(account.activity);
-    });
+  var accountId = req.params.id == 'me'
+                     ? req.session.accountId
+                     : req.params.id;
+  models.Account.findById(accountId, function(account) {
+    res.send(account.activity);
+  });
 });
+
 app.get('/accounts/:id/status', function(req, res) {
-    var accountId = req.params.id == 'me' ? req.session.accountId : req.params.id;
-    models.Account.findById(accountId, function(account) {
-        res.send(account.status);
-    });
+  var accountId = req.params.id == 'me'
+                     ? req.session.accountId
+                     : req.params.id;
+  models.Account.findById(accountId, function(account) {
+    res.send(account.status);
+  });
 });
+
 app.post('/accounts/:id/status', function(req, res) {
-    var accountId = req.params.id == 'me' ? req.session.accountId : req.params.id;
-    models.Account.findById(accountId, function(account) {
-        status = {
-            name: account.name,
-            status: req.param('status', '')
-        };
-        account.status.push(status);
-        // Push the status to all friends
-        account.activity.push(status);
-        account.save(function(err) {
-            if (err) {
-                console.log('Error saving account: ' + err);
-            }
-        });
+  var accountId = req.params.id == 'me'
+                     ? req.session.accountId
+                     : req.params.id;
+  models.Account.findById(accountId, function(account) {
+    status = {
+      name: account.name,
+      status: req.param('status', '')
+    };
+    account.status.push(status);
+
+    // Push the status to all friends
+    account.activity.push(status);
+    account.save(function (err) {
+      if (err) {
+        console.log('Error saving account: ' + err);
+      }
     });
-    res.send(200);
+  });
+  res.send(200);
 });
+
+app.delete('/accounts/:id/contact', function(req,res) {
+  var accountId = req.params.id == 'me'
+                     ? req.session.accountId
+                     : req.params.id;
+  var contactId = req.param('contactId', null);
+
+  // Missing contactId, don't bother going any further
+  if ( null == contactId ) {
+    res.send(400);
+    return;
+  }
+
+  models.Account.findById(accountId, function(account) {
+    if ( !account ) return;
+    models.Account.findById(contactId, function(contact,err) {
+      if ( !contact ) return;
+
+      models.Account.removeContact(account, contactId);
+      // Kill the reverse link
+      models.Account.removeContact(contact, accountId);
+    });
+  });
+
+  // Note: Not in callback - this endpoint returns immediately and
+  // processes in the background
+  res.send(200);
+});
+
+app.post('/accounts/:id/contact', function(req,res) {
+  var accountId = req.params.id == 'me'
+                     ? req.session.accountId
+                     : req.params.id;
+  var contactId = req.param('contactId', null);
+
+  // Missing contactId, don't bother going any further
+  if ( null == contactId ) {
+    res.send(400);
+    return;
+  }
+
+  models.Account.findById(accountId, function(account) {
+    if ( account ) {
+      models.Account.findById(contactId, function(contact) {
+        models.Account.addContact(account, contact);
+
+        // Make the reverse link
+        models.Account.addContact(contact, account);
+        account.save();
+      });
+    }
+  });
+
+  // Note: Not in callback - this endpoint returns immediately and
+  // processes in the background
+  res.send(200);
+});
+
 app.get('/accounts/:id', function(req, res) {
-    var accountId = req.params.id == 'me' ? req.session.accountId : req.params.id;
-    models.Account.findById(accountId, function(account) {
-        res.send(account);
-    });
+  var accountId = req.params.id == 'me'
+                     ? req.session.accountId
+                     : req.params.id;
+  models.Account.findById(accountId, function(account) {
+    if ( accountId == 'me' || models.Account.hasContact(account, req.session.accountId) ) {
+      account.isFriend = true;
+    }
+    res.send(account);
+  });
 });
+
 app.post('/forgotpassword', function(req, res) {
-    var hostname = req.headers.host;
-    var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
-    var email = req.param('email', null);
-    if (null == email || email.length < 1) {
-        res.send(400);
-        return;
+  var hostname = req.headers.host;
+  var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
+  var email = req.param('email', null);
+  if ( null == email || email.length < 1 ) {
+    res.send(400);
+    return;
+  }
+
+  models.Account.forgotPassword(email, resetPasswordUrl, function(success){
+    if (success) {
+      res.send(200);
+    } else {
+      // Username or password not found
+      res.send(404);
     }
-    models.Account.forgotPassword(email, resetPasswordUrl, function(success) {
-        if (success) {
-            res.send(200);
-        }
-        else {
-            // Username or password not found
-            res.send(404);
-        }
-    });
+  });
 });
+
+app.post('/contacts/find', function(req, res) {
+  var searchStr = req.param('searchStr', null);
+  if ( null == searchStr ) {
+    res.send(400);
+    return;
+  }
+
+  models.Account.findByString(searchStr, function onSearchDone(err,accounts) {
+    if (err || accounts.length == 0) {
+      res.send(404);
+    } else {
+      res.send(accounts);
+    }
+  });
+});
+
 app.get('/resetPassword', function(req, res) {
-    var accountId = req.param('account', null);
-    res.render('resetPassword.jade', {
-        locals: {
-            accountId: accountId
-        }
-    });
+  var accountId = req.param('account', null);
+  res.render('resetPassword.jade', {locals:{accountId:accountId}});
 });
+
 app.post('/resetPassword', function(req, res) {
-    var accountId = req.param('accountId', null);
-    var password = req.param('password', null);
-    if (null != accountId && null != password) {
-        models.Account.changePassword(accountId, password);
-    }
-    res.render('resetPasswordSuccess.jade');
+  var accountId = req.param('accountId', null);
+  var password = req.param('password', null);
+  if ( null != accountId && null != password ) {
+    models.Account.changePassword(accountId, password);
+  }
+  res.render('resetPasswordSuccess.jade');
 });
 
   
--- public/js/views/login.js
+++ public/js/views/login.js
@@ -1,27 +1,30 @@
-define(['Underscore', 'Backbone','text!templates/login.html'], function( _, Backbone, loginTemplate) {
-    var loginView = Backbone.View.extend({
-        el: $('#content'),
-        events: {
-            "submit form": "login"
-        },
-        login: function() {
-            var datosLogin = {
-                email: $('input[name=email]').val(),
-                password: $('input[name=password]').val()
-            };
-            console.log(datosLogin);
-            $.post('/login', datosLogin, function(data) {
-                console.log(data);
-            }).error(function() {
-                $("#error").text('Unable to login.');
-                $("#error").slideDown();
-            });
-            return false;
-        },
-        render: function() {
-            this.$el.html(loginTemplate);
-            $("#error").hide();
-        }
-    });
-    return loginView;
+define(['SocialNetView', 'text!templates/login.html'], function(SocialNetView, loginTemplate) {
+  var loginView = SocialNetView.extend({
+    requireLogin: false,
+
+el: $('#content'),
+
+    events: {
+      "submit form": "login"
+    },
+
+    login: function() {
+      $.post('/login',
+        this.$('form').serialize(), function(data) {
+        window.location.hash = 'index';
+      }).error(function(){
+        $("#error").text('Unable to login.');
+        $("#error").slideDown();
+      });
+      return false;
+    },
+
+    render: function() {
+      this.$el.html(loginTemplate);
+      $("#error").hide();
+      $("input[name=email]").focus();
+    }
+  });
+
+  return loginView;
 });
